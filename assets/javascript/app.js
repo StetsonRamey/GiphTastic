f32386c64761e9ec60e5639bf565cdf6cea18c00
Stetson Ramey <stetson.ramey@gmail.com>
2018-02-21 08:52:36 -0600

    gifs load on click, animate on click works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,19 +2,15 @@
 // ===================================================
 var key = "ZeKnEFlNSXBStYoC3CKTGrP28TaRnps1";
 var gifLimit = "&limit=10";
-var gifURL = "https://api.giphy.com/v1/gifs/search?q=";
+var baseURL = "https://api.giphy.com/v1/gifs/search?q=";
 // var gifURL = "https://api.giphy.com/v1/gifs/search?q=gosling&api_key=" + key + "&limit=10";
 var topics = ["gosling", "cats", "disney", "mustache", "babies", "falling", "will ferrell", "fail"];
 
-// so if there's a space in the array string, have to replace that with an & for API URL
-$.each(topics, function() {
-  var urlTopics = (this.split(" ").join("&"));
-  console.log(urlTopics);
-});
 
 
 // FUNCTIONS
 // ===================================================
+// make the buttons display on the screen
 function renderButtons() {
   $("#buttons-view").empty();
 
@@ -26,55 +22,75 @@ function renderButtons() {
     $("#buttons-view").append(a);
   }
 }
+// call the gifs in
+function getGifURL() {
+  // now based on button clicked, I have to finish the URL
+  $(".btn").click(function() {
+    var qSearch = $(this).attr("data-name");
+    // testing & debugging
+    // console.log(qSearch);
+    // have to remove spaces if there are any
+    var qSearchURL = qSearch.split(" ").join("+");
+    // testing & debugging
+    // console.log(qSearchURL);
+    // put the URL together
+    var gifURL = baseURL + qSearchURL + "&api_key=" + key + gifLimit;
+    // testing and debugging
+    // console.log(gifURL);
 
-function getGifs() {
-  // hit the APIs
-  $.ajax({
-    url: gifURL,
-    method: "GET"
-  }).then(function(response) {
+    function getGifs() {
+      // hit the APIs
+      $.ajax({
+        url: gifURL,
+        method: "GET"
+      }).then(function(response) {
 
-    for (var i = 0; i < 10; i++) {
-      var staticURL = response.data[i].images.fixed_height_small_still.url;
-      // console.log(gifURL);
-      var still = $("<img>");
-      still.attr("src", staticURL);
-      still.addClass("dynamic");
-      $("#gif").append(still);
+        for (var i = 0; i < 10; i++) {
+          var staticURL = response.data[i].images.fixed_height_small_still.url;
+          // console.log(gifURL);
+          var still = $("<img>");
+          still.attr("src", staticURL);
+          still.addClass("static");
+          $("#gif").append(still);
+        }
+      });
     }
-
-    // create a var for the state of the gif
-    var clickedGif = false;
-    // run the logic when the user clicks
-    $(".dynamic").click(function() {
-      // store the url of the clicked gif in a variable
-      var clickedURL = $(this).attr("src");
-      // testing and debugging
-      console.log(clickedURL);
-      // run a test so we know what state the gif is in
-      if (!clickedGif) {
-        // if the gif is static, animate it and set the state to running
-        var dynamicURL = clickedURL.split('_s').join('');
-        // testing and debugging
-        console.log(dynamicURL);
-        // push it to the screen
-        $(this).attr("src", dynamicURL);
-        // set the state
-        clickedGif = true;
-      } else {
-        // else if the gif is running, set it back to static
-        staticURL = clickedURL.split('.gif').join('_s.gif');
-        // testing and debugging
-        console.log(staticURL);
-        // push it to the screen
-        $(this).attr("src", staticURL);
-        // set the state
-        clickedGif = false;
-      }
-    })
-  })
+    getGifs();
+  });
 }
+
+// function to set the state of the gifs, whether they are static or dynamic
+function animate() {
+  // store the url of the clicked gif in a variable
+  var clickedURL = $(this).attr("src");
+  var state = $(this).attr("class");
+  // testing and debugging
+  console.log(state);
+  // run a test so we know what state the gif is in
+  if (state === 'static') {
+    // if the gif is static, animate it and set the state to running
+    var dynamicURL = clickedURL.split('_s').join('');
+    // push it to the screen
+    $(this).attr("src", dynamicURL);
+    // set the state
+    $(this).attr("class", "dynamic");
+  } else if (state === 'dynamic') {
+    // else if the gif is running, set it back to static
+    staticURL = clickedURL.split('.gif').join('_s.gif');
+    // push it to the screen
+    $(this).attr("src", staticURL);
+    // set the state
+    $(this).attr("class", "static");
+  }
+}
+
+
+
 // GAME LOGIC
 // ===================================================
-
 renderButtons();
+
+getGifURL();
+
+$(document).on("click", ".static", animate);
+$(document).on("click", ".dynamic", animate);
